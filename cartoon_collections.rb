b156f273254ff61215af8c8e30e2671e9ea57ba8
Tatyana Scotce <tscotce@gmail.com>
2018-02-22 22:32:11 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,9 +5,17 @@ def roll_call_dwarves(list)
 end
 
 def summon_captain_planet(array)
+<<<<<<< HEAD
   array.collect do |word|
     "#{word.capitalize}!"
   end
+=======
+  capitalized_array = []
+  array.each do |word|
+    capitalized_array << "#{word.capitalize}!"
+  end
+  capitalized_array
+>>>>>>> 6f9444595ddfe94b503584ecd436e5fbfa18d62a
 end
 
 def long_planeteer_calls(array)
@@ -19,5 +27,11 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+<<<<<<< HEAD
   array.detect {|word| word == "cheddar" || word == "gouda" || word == "camembert"}
+=======
+  puts cheese_types if array.include?(cheese_types) == true
+  else nil
+  end
+>>>>>>> 6f9444595ddfe94b503584ecd436e5fbfa18d62a
 end
